29aab82110dc2878b26ab7b290913f593f501c57
Hadi Kashif <f2023266412@umt.edu.pk>
2025-09-01 17:53:11 +0500

    Add files via upload

---
+++ Operator and Variables/Typecasting.py
@@ -0,0 +1,14 @@
+# msg= "This is msg"
+# time = 3.33
+# location = 9
+
+# print (type(msg))
+# print (type(time))
+# print (type(location))
+ 
+ 
+#  TAKING INPUT AND THEN FINSS ITS TYPE 
+a = input("Enter the name: ")    
+b = int (input("Enter the no: "))    
+print (type (a))
+print (type (b))
---
+++ Operator and Variables/input_funtion.py
@@ -0,0 +1,13 @@
+# a = input("Enter  number for A ")
+# b = input("Enter  number of B ")
+
+# print("A is : ", a)
+# print("B is : ", b)
+
+
+a = int (input("Enter  number for A "))
+b = int (input("Enter  number of B "))
+
+print("A is : ", a)
+print("B is : ", b)
+print("Sum of A B is: ", a + b  )
