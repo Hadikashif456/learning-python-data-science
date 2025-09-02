3e89cf4019d0624ebfda8f628f977df00d64d20e
Hadi Kashif <f2023266412@umt.edu.pk>
2025-09-02 16:38:01 +0500

    Add files via upload

---
+++ Operator and Variables/Strings.py
@@ -0,0 +1,18 @@
+name = 'Hadi'
+Name1 = "Hadi"
+Name2 = '''Hadi '''
+
+# short_name = name [0:2]
+# print (name)
+# print (Name1)
+# print (Name2)
+# print (short_name)
+
+# print (len(name))
+
+# print (name.endswith("L"))
+
+# a = "Hadi is a boy \n but He is achieving is dreams"
+# print (a)
+
+print(f" Welcome to this page {name }")
