90ec33905606c69a1a2ba1ca56f14c23bc37083c
Hadi Kashif <f2023266412@umt.edu.pk>
2025-09-25 22:53:59 +0500

    Add files via upload

---
+++ Conditional Expressions/Loops.py
@@ -0,0 +1,72 @@
+# # for loop
+# for i in range(1,6):
+#     print(i)
+#     #  1->5
+
+
+# #  Table of 5
+# for i in range(0,10):
+#         print("the multiple of", i , "X 5 =", i*5)
+    
+    
+# For loop in List
+# List_of_names = ["Hadi","Hadia","Shafay","Shafia"]
+# List_of_names = [99,1,26,254]
+
+# print(type(List_of_names))
+# print(List_of_names)
+
+# for i in List_of_names:
+#     print(i)
+    
+    
+    
+# For loop in Tuple
+
+# Tuple_for_loop= {12,56,32,789,45}
+# for i in Tuple_for_loop:
+#      print(i)
+     
+    #  else (We use else at the end of for loop and i print/ excuetes only when loop ends!)
+# else:
+#     print("Loop is done!")
+
+
+# break statment
+# for i in range(0,5):
+#     print(i)
+#     i+=1 
+#     if(i==3):
+#         break
+    
+    
+    
+# # Continue statment
+# for i in range(20):
+#     if(i==3):
+#         continue
+#     print(i)
+
+
+
+#  Practise questions
+
+# l= ["Hadi", "Eva", "Laura", "Ferrara","Hardis"]
+
+# for i in l:
+#     if(i.startswith("H")):
+#         print("Greetings to you!",i)
+
+
+# for i in range (5):
+#     print(i)
+
+# for i in range(0,4):
+#     print("*")
+#     for j in range(i):
+#         print("*", end="")
+# print()
+
+
+# for i in range (10, 0,-1):
+#     print(i)
