3b0cf357b3908537c3e1e6cbce6e31745b6ab029
Hadi Kashif <f2023266412@umt.edu.pk>
2025-09-25 22:35:49 +0500

    Add files via upload

---
+++ Operator and Variables/Dicticnaory&sets.py
@@ -0,0 +1,54 @@
+# Employees ={
+#                 "Hadi" :"Engineer",
+#                 "Age " : 21,
+#                 "City" : "Sicily"
+                               
+#     } 
+
+# # print(Employees)
+# # print ( Employees["Hadi"])
+
+# # print(Employees.items())
+# # print(Employees.keys())
+
+# # Employees.update( { "Hadi": 95} )  # update the value 
+# # print(Employees.items())
+
+
+# # print (Employees.get(("Hadi")))
+# # print(Employees ["Hadi"]) 
+  
+#              #Empty Dictionary-->  
+#             #  Dict = {}
+
+
+
+# #                                ****Now Set****
+# #S ={1,23,3,9,1,9,2}
+# #print(S)
+ 
+# #S.add(1003)
+# #print(S, type(S))
+
+
+# s= {3,1,6,5,9}
+# print(s)
+# print(type(s))
+# #  Methods 
+
+# # 1. ADD
+# s.add(999)
+# print(s)
+
+
+# Questions
+
+words ={
+    "Book" : "kitab",
+    "Ladder" : "Siri",
+    "Cloud" : "badal"
+}
+
+input_word = (input("Enter the word you want to search for meaning: "))
+print (words[input_word])
+
